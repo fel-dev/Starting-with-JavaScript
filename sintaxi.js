e1477a7643a28b1125bf4c6744a44fe5ac719fa3
Felipe Carneiro <correaflp@gmail.com>
2023-12-12 11:19:22 -0300

    2.1. Sintaxe básica

--- sintaxi.js
+++ sintaxi.js
@@ -1,1 +1,37 @@
-console.log("test");
+// statment ends with ";"
+var x = 15;
+var y = 12;
+// Values: literals and variables
+// Literal: numbers
+10; // integer
+10.5; // floating-point
+"olá mundo"; // string
+'olá mundo'; // string
+// Variable: declare with "let" or "var"
+var myVariable = undefined;
+// Operators: + - * / % **
+x = 10;
+10 + 5;
+20 - 10;
+40 * 5;
+20 / 10;
+// Expressions: operators + values
+y + 10;
+20 * 30;
+x * y;
+// <- this is a comment line, it will be ignored by the compiler.
+/* This is a comment block
+c
+o
+m
+m
+n
+t
+*/
+// Indentifier
+// Valid "var" starts with letter, $ or _
+var $1;
+var _1;
+var hello;
+var helloWorld;
+// Invalid "var" starts with number
--- sintaxi.ts
+++ sintaxi.ts
@@ -1,1 +1,53 @@
-console.log("test");
+// statment ends with ";"
+var x: number = 15;
+var y: number = 12;
+
+// Values: literals and variables
+
+// Literal: numbers
+10; // integer
+10.5; // floating-point
+"olá mundo"; // string
+'olá mundo'; // string
+
+// Variable: declare with "let" or "var"
+
+var myVariable: any = undefined;
+
+// Operators: + - * / % **
+
+x = 10;
+
+10 + 5;
+20 - 10;
+40 * 5;
+20 / 10;
+
+// Expressions: operators + values
+
+y + 10;
+20 * 30;
+x * y;
+
+// <- this is a comment line, it will be ignored by the compiler.
+
+/* This is a comment block
+c
+o
+m
+m
+n
+t
+*/
+
+// Indentifier
+
+// Valid "var" starts with letter, $ or _
+
+var $1: any;
+var _1: any;
+var hello: any;
+var helloWorld: any;
+
+// Invalid "var" starts with number
+
