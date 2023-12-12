955feb81b00c531a3c92e8fb70f0b5198d8b1670
Felipe Carneiro <correaflp@gmail.com>
2023-12-12 16:49:42 +0000

    3.1. Declaração

--- 3.1. Declaração/declaration.js
+++ 3.1. Declaração/declaration.js
@@ -1,3 +1,21 @@
+// Hoisting - Move declaration to top -- Only works with "function" declaration
+// faz o hoisting da variável, mas não da atribuição com a função
+sayHi("Joana D'Arc");
+// Traditional function
 function sayHi(name) {
-    return "Hi " + name;
+    console.log("Hi " + name);
 }
+sayHi("Mary");
+// Anonymous function
+var sayHello = function (name) {
+    console.log("Hello " + name);
+};
+sayHello("Sebastian");
+// Constructor function (new) - Not recommended
+var sayBay = new Function("name", "console.log('Bye ' + name);");
+sayBay("John");
+// Arrow function
+var sayGoodMorning = (name) => {
+    console.log("Good morning " + name);
+};
+sayGoodMorning("Clara");
