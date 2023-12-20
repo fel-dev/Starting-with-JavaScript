453d8886697703c36a2847215a0e7946189985c7
Felipe Carneiro <correaflp@gmail.com>
2023-12-20 17:32:57 +0000

    Refactor array and object declarations

--- 4 - Objetos/4.3. Arrays/arrays.js
+++ 4 - Objetos/4.3. Arrays/arrays.js
@@ -1,5 +1,16 @@
 "use strict";
-var stark = ["Ned", 2, function () { }, [], true];
-for (var i in stark) {
-    console.log(stark[i]);
+const starks = [
+    ["Robb", 14],
+    ["Sansa", 11],
+    ["Arya", 9],
+    ["Bran", 7]
+];
+for (let i in starks) {
+    console.log(starks[i]);
 }
+console.log('\n------------------\nAccessing a element inside a object:');
+const stark = {
+    name: "Ned",
+    age: 40
+};
+console.log(stark.name);
--- 4 - Objetos/4.3. Arrays/arrays.ts
+++ 4 - Objetos/4.3. Arrays/arrays.ts
@@ -1,6 +1,19 @@
-var stark : any[] = ["Ned", 2, function(){}, [], true];
+const starks: [string, number][] = [
+    ["Robb", 14],
+    ["Sansa", 11],
+    ["Arya", 9],
+    ["Bran", 7]
+];
 
-for (var i in stark) {
-    console.log(stark[i]);
-
+for (let i in starks) {
+    console.log(starks[i]);
 }
+
+console.log('\n------------------\nAccessing a element inside a object:');
+
+const stark: { name: string, age: number } = {
+    name: "Ned",
+    age: 40
+};
+
+console.log(stark.name);
