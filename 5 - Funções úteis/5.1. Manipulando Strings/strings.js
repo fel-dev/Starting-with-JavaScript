9167f1d59f0bb8832810b63aaf95d55fa3294286
Felipe Carneiro <correaflp@gmail.com>
2023-12-20 23:44:47 +0000

    pedaço da string

--- 5 - Funções úteis/5.1. Manipulando Strings/strings.js
+++ 5 - Funções úteis/5.1. Manipulando Strings/strings.js
@@ -1,3 +1,3 @@
 "use strict";
 var s = "The winter is coming. It's really coming!";
-console.log(s.lastIndexOf("coming"));
+console.log(s.slice(4, 10)); // winter
--- 5 - Funções úteis/5.1. Manipulando Strings/strings.ts
+++ 5 - Funções úteis/5.1. Manipulando Strings/strings.ts
@@ -1,2 +1,2 @@
 var s: string = "The winter is coming. It's really coming!";
-console.log(s.lastIndexOf("coming"));
+console.log(s.slice(4, 10)); // winter
