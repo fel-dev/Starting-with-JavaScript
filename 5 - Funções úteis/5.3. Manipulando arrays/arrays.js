1be29b83643c026f6abb496638dea06002cee25b
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 02:17:02 +0000

    Add array join function

--- 5 - Funções úteis/5.3. Manipulando arrays/arrays.js
+++ 5 - Funções úteis/5.3. Manipulando arrays/arrays.js
@@ -1,3 +1,4 @@
 "use strict";
 var array = "Ned, Jon, Robb, Bran, Rickon".split(", ");
 console.log(array.toString()); // Ned,Jon,Robb,Bran,Rickon
+console.log(array.join(", ")); // Ned Jon Robb Bran Rickon
--- 5 - Funções úteis/5.3. Manipulando arrays/arrays.ts
+++ 5 - Funções úteis/5.3. Manipulando arrays/arrays.ts
@@ -1,3 +1,5 @@
 var array: string[] = "Ned, Jon, Robb, Bran, Rickon".split(", ");
 
 console.log(array.toString()); // Ned,Jon,Robb,Bran,Rickon
+console.log(array.join(", ")); // Ned Jon Robb Bran Rickon
+
