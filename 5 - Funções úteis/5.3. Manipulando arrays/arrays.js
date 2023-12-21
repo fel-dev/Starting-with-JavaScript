7b2fde16d646ba85a064e9a5e854a175ea3961af
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 02:33:43 +0000

    Changing elements in an array

--- 5 - Funções úteis/5.3. Manipulando arrays/arrays.js
+++ 5 - Funções úteis/5.3. Manipulando arrays/arrays.js
@@ -6,7 +6,9 @@ array.push("Sansa"); // ["Ned", "Jon", "Robb", "Bran", "Rickon", "Sansa"]
 array.unshift("Catelyn");
 console.log(array);
 var element = array.pop();
-// remove the first element
 element = array.shift();
 console.log(element); // Catelyn
 console.log(array); // ["Ned", "Jon", "Robb", "Bran", "Rickon"]
+// Changing elements
+array[1] = "Jon Snow"; // ["Ned", "Jon Snow", "Robb", "Bran", "Rickon"]
+console.log(array);
--- 5 - Funções úteis/5.3. Manipulando arrays/arrays.ts
+++ 5 - Funções úteis/5.3. Manipulando arrays/arrays.ts
@@ -11,8 +11,11 @@ console.log(array);
 
 var element: string | undefined = array.pop(); 
 
-// remove the first element
 element = array.shift();
 
 console.log(element); // Catelyn
 console.log(array); // ["Ned", "Jon", "Robb", "Bran", "Rickon"]
+
+// Changing elements
+array[1] = "Jon Snow"; // ["Ned", "Jon Snow", "Robb", "Bran", "Rickon"]
+console.log(array);
