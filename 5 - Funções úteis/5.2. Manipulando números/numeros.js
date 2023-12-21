f05142ffbf2403d4ce401eceaefeaa8674fd41b2
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 01:26:26 +0000

    Fix number precision and add hexadecimal example

--- 5 - Funções úteis/5.2. Manipulando números/numeros.js
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.js
@@ -1,6 +1,7 @@
 "use strict";
 var x = 456e5; //45600000
 var y = 456e-6; //0.000456
-// JS precision is 15-16 digits. Beyound called overflow or underflow
-var z = 0.99999999999999999; // underflow
-console.log(z); // 1
+var z = 0.99999999999999999; // 1
+// Hexadecimal
+var a = 0x100;
+console.log(a);
--- 5 - Funções úteis/5.2. Manipulando números/numeros.ts
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.ts
@@ -1,7 +1,10 @@
 var x: number = 456e5; //45600000
 var y: number = 456e-6; //0.000456
 
-// JS precision is 15-16 digits. Beyound called overflow or underflow
+var z: number = 0.99999999999999999; // 1
 
-var z: number = 0.99999999999999999; // underflow
-console.log(z); // 1
+// Hexadecimal
+var a: number = 0x100;
+
+console.log(a); 
+
