efc151abc750ada67eb7c397df3f658d6cdef49d
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 00:41:22 +0000

    eg. number overflow

--- 5 - Funções úteis/5.2. Manipulando números/numeros.js
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.js
@@ -2,5 +2,5 @@
 var x = 456e5; //45600000
 var y = 456e-6; //0.000456
 // JS precision is 15-16 digits. Beyound called overflow or underflow
-var z = 123456789012345;
-console.log(z); //123456789012345
+var z = 9999999999999999; // overflow
+console.log(z); // 10000000000000000
--- 5 - Funções úteis/5.2. Manipulando números/numeros.ts
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.ts
@@ -3,5 +3,5 @@ var y: number = 456e-6; //0.000456
 
 // JS precision is 15-16 digits. Beyound called overflow or underflow
 
-var z: number = 123456789012345; 
-console.log(z); //123456789012345
+var z: number = 9999999999999999; // overflow
+console.log(z); // 10000000000000000
