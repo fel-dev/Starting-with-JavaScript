217266e960a0885ab0439b526c675d8fe557664b
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 02:20:25 +0000

    new elements can be added to the end of an array by using the push method

--- 5 - Funções úteis/5.3. Manipulando arrays/arrays.js
+++ 5 - Funções úteis/5.3. Manipulando arrays/arrays.js
@@ -1,4 +1,7 @@
 "use strict";
 var array = "Ned, Jon, Robb, Bran, Rickon".split(", ");
 console.log(array.toString()); // Ned,Jon,Robb,Bran,Rickon
-console.log(array.join(", ")); // Ned Jon Robb Bran Rickon
+console.log(array.join(" | ")); // Ned | Jon | Robb | Bran | Rickon
+// new elements can be added to the end of an array by using the push method
+array.push("Sansa");
+console.log(array); // [ 'Ned', 'Jon', 'Robb', 'Bran', 'Rickon', 'Sansa' ]
--- 5 - Funções úteis/5.3. Manipulando arrays/arrays.ts
+++ 5 - Funções úteis/5.3. Manipulando arrays/arrays.ts
@@ -1,5 +1,10 @@
 var array: string[] = "Ned, Jon, Robb, Bran, Rickon".split(", ");
 
 console.log(array.toString()); // Ned,Jon,Robb,Bran,Rickon
-console.log(array.join(", ")); // Ned Jon Robb Bran Rickon
+console.log(array.join(" | ")); // Ned | Jon | Robb | Bran | Rickon
+
+// new elements can be added to the end of an array by using the push method
+array.push("Sansa");
+
+console.log(array); // [ 'Ned', 'Jon', 'Robb', 'Bran', 'Rickon', 'Sansa' ]
 
