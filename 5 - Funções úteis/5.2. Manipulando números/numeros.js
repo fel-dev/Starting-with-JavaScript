5cd675b280aabc767b8671ce2a896e3837e8c88c
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 01:48:21 +0000

    Refactor number manipulation code to use octal notation

--- 5 - Funções úteis/5.2. Manipulando números/numeros.js
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.js
@@ -2,6 +2,7 @@
 var x = 456e5; //45600000
 var y = 456e-6; //0.000456
 var z = 0.99999999999999999; // 1
-// Hexadecimal
 var a = 0xFFF; // 4095
-console.log(a); // FFF
+// Octal
+var b = 0o7777;
+console.log(b); // 4095
--- 5 - Funções úteis/5.2. Manipulando números/numeros.ts
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.ts
@@ -3,8 +3,11 @@ var y: number = 456e-6; //0.000456
 
 var z: number = 0.99999999999999999; // 1
 
-// Hexadecimal
 var a: number = 0xFFF; // 4095
 
-console.log(a); // FFF
 
+// Octal
+
+var b: number = 0o7777;
+
+console.log(b); // 4095
