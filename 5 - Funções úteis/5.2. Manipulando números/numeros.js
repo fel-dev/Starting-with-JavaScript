7c70d91d6b3a2d1f50a376f3d3b98a297bd9f2df
Felipe Carneiro <correaflp@gmail.com>
2023-12-21 00:38:20 +0000

    Add precision example for JS numbers

--- 5 - Funções úteis/5.2. Manipulando números/numeros.js
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.js
@@ -1,3 +1,6 @@
 "use strict";
 var x = 456e5; //45600000
 var y = 456e-6; //0.000456
+// JS precision is 15-16 digits. Beyound called overflow or underflow
+var z = 123456789012345;
+console.log(z); //123456789012345
--- 5 - Funções úteis/5.2. Manipulando números/numeros.ts
+++ 5 - Funções úteis/5.2. Manipulando números/numeros.ts
@@ -1,2 +1,7 @@
 var x: number = 456e5; //45600000
 var y: number = 456e-6; //0.000456
+
+// JS precision is 15-16 digits. Beyound called overflow or underflow
+
+var z: number = 123456789012345; 
+console.log(z); //123456789012345
